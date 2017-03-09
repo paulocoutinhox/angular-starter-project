fd3092b9c175441a19eb9c1bc5964520dc285dbc
Paulo Coutinho <paulo@prsolucoes.com>
2017-03-09 17:06:34 -0300

    updated e2e tests, travis script and test deps

--- tests/e2e/protractor.conf.js
+++ tests/e2e/protractor.conf.js
@@ -1,14 +1,20 @@
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
+	browserName: 'chrome',
 	framework: 'jasmine',
 	specs: ['../../tests/e2e/**/*.spec.js'],
 	useAllAngular2AppRoots: true,
 	directConnect: true,
 	allScriptsTimeout: 80000,
 	capabilities: {
-		'browserName': 'firefox'
+		'browserName': 'chrome'
 	},
 	jasmineNodeOpts: {
 		showColors: true,
 		defaultTimeoutInterval: 80000
+	},
+	onPrepare: function () {
+		jasmine.getEnv().addReporter(new SpecReporter());
 	}
 };
